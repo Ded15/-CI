c31eb9124f494675598e9505f1460b4ce5fe9192
Julia Hardikova <iylia.hardikova@mail.ru>
2022-12-24 22:08:49 +0300

    schema.json

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -22,8 +22,7 @@ class MobileBankApiTestV4 {
       .then()
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-          .body("[0].currency", equalTo("RUR"))
-          .body("[1].currency", equalTo("USD"))
+          .body("[0].currency", equalTo("RUB"))
       ;
     }
 }
